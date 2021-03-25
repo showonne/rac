d0329d6140e402e942ff0f7d52c49b698d05c3bc
showonne <918677896@qq.com>
2021-03-25 22:35:57 +0800

    fix: diff bug

--- examples/src/diff.tsx
+++ examples/src/diff.tsx
@@ -1,16 +1,34 @@
-import { h, render, useState } from '../../src'
+import { h, render, useEffect, useState } from '../../src'
+import { random, shuffle } from 'lodash'
+
+const genSource = () => {
+  const source = shuffle(Array(random(0, 10)).fill('').map((_, index) => index))
+  console.log(source)
+  return source
+}
 
 function App() {
-  const [keys, setKeys] = useState([1, 2, 3, 4])
+  const [keys, setKeys] = useState([1, 2, 3, 4, 5])
+  // const [keys, setKeys] = useState([3, 4, 2, 1, 0])
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setKeys(genSource())
+  //   }, 100)
+  // }, [])
  
   return (
     <div>
+      <p>
+        {keys.join(',')}
+      </p>
       <ul>
         {
-          keys.map(key => key === 1 ? <div key={key}>key 1 div</div> : <li key={key}>{key}</li>)
+          keys.map(key => key === 1 ? <div style="color: red;" key={`${key}`}>key 1 div</div> : <li key={`${key}`}>{key}</li>)
         }
       </ul>
-      <button onClick={() => setKeys([2, 3, 4, 1])}>Change key</button>
+      <button onClick={() => setKeys(genSource())}>Change key</button>
+      {/* <button onClick={() => setKeys([0, 3, 1, 2])}>Change key</button> */}
       <button onClick={() => setKeys(prev => prev.concat([prev.length + 1]))}>Add Item</button>
     </div>
   )
