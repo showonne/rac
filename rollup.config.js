5b707881e9a4a029348ca144f349932c5a7cb5b3
showonne <918677896@qq.com>
2021-04-10 14:16:06 +0800

    misc: add banner

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,21 @@ const path = require('path')
 const typescript = require('rollup-plugin-typescript2')
 const { terser } = require('rollup-plugin-terser')
 const eslint = require('@rollup/plugin-eslint')
+const version = require('./package.json').version
+
+const banner = `
+/*!
+ * Rac.js v${version}
+ * (c) 2021-${new Date().getFullYear()} Showonne
+ * Released under the MIT License.
+ */
+`
 
 export default {
   input: 'src/index.ts',
   output: [
-    { file: 'dist/rac.umd.js', format: 'umd', name: 'rac', sourcemap: true },
-    { file: 'dist/rac.js', format: 'esm', sourcemap: true }
+    { file: 'dist/rac.umd.js', format: 'umd', name: 'rac', sourcemap: true, banner },
+    { file: 'dist/rac.js', format: 'esm', sourcemap: true, banner }
   ],
   plugins: [
     terser(),
