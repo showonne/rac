b62b84b234b2c20807e3f0ebb88ba905c112820d
陈景辉 <chenjinghui@192.168.0.118>
2022-08-06 20:30:42 +0800

    chore: update deps

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { h, Fragment, lazy, Suspense } from './h'
 export { render } from './reconciler'
-export { 
+export {
   useState, useReducer, useMemo, useCallback, useRef, useEffect, useLayoutEffect
 } from './hooks'
